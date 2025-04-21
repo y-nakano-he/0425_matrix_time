b220789821f1ad13e9dfc98a881b150276a3d0ce
Y-Nakano-he <y.nk40458359@gmail.com>
2025-04-21 14:18:45 +0900

    malloc_matrix実装

--- matrix03.c
+++ matrix03.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #define N 1000
 
+//matrix_product：N*Nの行列の乗算を行う
 void matrix_product(double A[N][N], double B[N][N], double C[N][N]){
 	//インデックスを作成
 	int i,j,k;
@@ -21,28 +22,57 @@ void matrix_product(double A[N][N], double B[N][N], double C[N][N]){
 	}
 }
 
-void rand_matrix(double C[N][N]){
+void rand_matrix(double **C, int N){
 	int i,j;
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++){
-			C[i][j] = rand() % 10;
+			C[i][j] = rand();
 		}
 	}
 }
+/*****************************************
+*malloc_matrix関数：行列のメモリを確保する	*
+*配列mat1[N]分のメモリ領域を確保		    *
+*→ mat1をN個並べた分のメモリ領域を確保    	*
+******************************************/
 
+double **malloc_matrix(int N){
+	//mat1：double型のポインタをN個持った「行ポインタの配列」
+	double **mat1 = malloc(N * sizeof(*mat1));
+	if(mat1 == NULL){
+		printf("Memory not allocated.\n");
+		exit(8);
+	}
+	for(int i = 0; i < N; i++){
+		mat1[i] = malloc(N * sizeof(mat1));
+		if(mat1 == NULL){
+			printf("Memory not allocated.\n");
+			exit(8);
+		}
+	}
+	return mat1;
+}
 
-double main(){
-	double A[N][N];
-	double B[N][N];
-	double C[N][N];
+int main(){
+	//mallocで行列を確保して作成
+	double **A = malloc_matrix(N);
+	double **B = malloc_matrix(N);
+	double **C = malloc_matrix(N);
 
+	//確保した型と領域に要素を挿入していく
 	rand_matrix(A);
 	rand_matrix(B);
 	rand_matrix(C);
 	
+	//行列演算のCPU時間を測定
 	clock_t begin = clock();
 	matrix_product(A, B, C);
 	clock_t end = clock();
+
+	//確保した領域を解放
+	free_matrix(A, N);
+	free_matrix(B, N);
+	free_matrix(C, N);
 
 	printf("CPU time = %lf sec\n", (double)(end - begin) / CLOCKS_PER_SEC);
 
