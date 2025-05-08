c997c2bfb8e7b8e88ded7350a62e322c268da50f
yuki nakano <y.nk40458359@gmail.com>
2025-05-08 11:30:45 +0900

    bugfix

--- matrix03.c
+++ matrix03.c
@@ -9,7 +9,7 @@
 *→配列mat1[N]1行分のメモリ領域を確保		*
 ******************************************/
 
-double **malloc_matrix1(int N){
+double **malloc_matrix(int N){
 	//double* mat1はmat1[0]を指す  
 	double **mat1 = malloc(N * sizeof(*mat1));
 	if(mat1 == NULL){
@@ -47,11 +47,47 @@ void matrix_ijk(double **m1, double **m2, double **m3, int N){
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N;j++){
 			for(int k = 0; k < N; k++){
-				double cij = 0.0;
-				cij += m1[i][k] * m2[k][j];
+				m3[i][j] += m1[i][k] * m2[k][j];
 			}
-			m3[i][j] = cij;
 		}
+	}
+}
+
+//matrix_ikj：N*Nの行列の乗算を行う i->k->j
+void matrix_ikj(double **m1, double **m2, double **m3, int N){
+	//i->j->kでループ
+	for(int i = 0; i < N; i++){
+		for(int k = 0; k < N;k++){
+			double 1ik = m1[i][k];
+			for(int j = 0; j < N; j++){
+				m3[i][j] += 1ik * m2[k][j];
+			}
+		}		
+	}
+}
+
+//matrix_jik：N*Nの行列の乗算を行う j->i->k
+void matrix_jik(double **m1, double **m2, double **m3, int N){
+	//j->i->kでループ
+	for(int j = 0; j < N; j++){
+		for(int i = 0; i < N;i++){
+			for(int k = 0; k < N; k++){
+				m3[i][j] += m1[i][k] * m2[k][j];
+			}
+		}		
+	}
+}
+
+//matrix_jki：N*Nの行列の乗算を行う j->k->i
+void matrix_jki(double **m1, double **m2, double **m3, int N){
+	//j->k->iでループ
+	for(int j = 0; j < N; j++){
+		for(int k = 0; k < N;k++){
+			double 2kj = m2[k][j];
+			for(int i = 0; i < N; i++){
+				m3[i][j] += m1[i][k] * 2kj;
+			}
+		}		
 	}
 }
 //matrix_kij：N*Nの行列の乗算を行う k->i->j
@@ -59,57 +95,22 @@ void matrix_kij(double **m1, double **m2, double **m3, int N){
 	//k→i→jでループ
 	for(int k = 0; k < N; k++){
 		for(int i = 0; i < N; i++){
-			double aik = m1[i][k];
+			double 1ik = m1[i][k];
 			for(int j = 0; j < N; j++){
-				m3[i][j] += aik * m2[k][j];
+				m3[i][j] += 1ik * m2[k][j];
 			}
 		}		
 	}
 }
-//matrix_ikj：N*Nの行列の乗算を行う i->k->j
-void matrix_ikj(double **m1, double **m2, double **m3, int N){
-	//i->j->kでループ
-	for(int i = 0; i < N; i++){
-		for(int k = 0; k < N;k++){
-			double aik = m1[i][k];
-			for(int j = 0; j < N; j++){
-				m3[i][j] += aik * m2[k][j];
-			}
-		}		
-	}
-}
-//matrix_jik：N*Nの行列の乗算を行う j->i->k
-void matrix_jik(double **m1, double **m2, double **m3, int N){
-	//j->i->kでループ
-	for(int j = 0; j < N; j++){
-		for(int i = 0; i < N;i++){
-			for(int k = 0; k < N; k++){
-				double cij += m1[i][k] * m2[k][j];
-			}
-			m3[i][j] = cij;
-		}		
-	}
-}
-//matrix_jki：N*Nの行列の乗算を行う j->k->i
-void matrix_jki(double **m1, double **m2, double **m3, int N){
-	//j->k->iでループ
-	for(int j = 0; j < N; j++){
-		for(int k = 0; k < N;k++){
-			double bkj = mat2[k][j];
-			for(int i = 0; i < N; i++){
-				mat3[i][j] += mat1[i][k] * bkj;
-			}
-		}		
-	}
-}
+
 //matrix_kji：N*Nの行列の乗算を行う k->j->i
 void matrix_kji(double **m1, double **m2, double **m3, int N){
 	//k->j->iでループ
 	for(int k = 0; k < N; k++){
 		for(int j = 0; j < N;j++){
-			double bkj = m2[k][j];
+			double 2kj = m2[k][j];
 			for(int i = 0; i < N; i++){
-				m3[i][j] += m1[i][k] * bkj;
+				m3[i][j] += m1[i][k] * 2kj;
 			}
 		}		
 	}
@@ -124,6 +125,7 @@ void free_matrix1(double **mat, int N){
 }
 
 int main(){
+	//ループ用に関数ポインタ配列を作成
 	void (*funcarray[])(double**, double**, double**, int) = {matrix_ijk, matrix_ikj, matrix_jik, matrix_jki, matrix_kij, matrix_kji};
 	FILE *fp = fopen("2dmatrix_product_time.csv", "w");
 	if(fp == NULL) {
@@ -135,17 +137,17 @@ int main(){
 	for(int N = 1000; N <= 10000; N += 1000){
 		printf("\nMatrix size: %d\n", N);
 		//mallocで行列を確保して作成
-		double **A = malloc_matrix1(N);
-		double **B = malloc_matrix1(N);
-		double **C = malloc_matrix1(N);
+		double **A = malloc_matrix(N);
+		double **B = malloc_matrix(N);
+		double **C = malloc_matrix(N);
 
 		rand_matrix(A, N);
 		rand_matrix(B, N);
 
 		for(int i = 0; i < 6; i++){
 			zero_matrix(C, N);
-			struct timespec begin, end;
-			clock_gettime(CLOCK_MONOTONIC, &begin);
+			struct timespec start, end;
+			clock_gettime(CLOCK_MONOTONIC, &start);
 			funcarray[i](A, B, C, N);
 			clock_gettime(CLOCK_MONOTONIC, &end);
 			double matrix_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
